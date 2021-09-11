32ac419b16aea5c9bfb5e32c8fb0cad8d1bcef76
Miikka Ylätalo <totally.mehis@gmail.com>
2021-09-11 19:05:17 +0300

    Minify HTML

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,14 @@
+const htmlmin = require("html-minifier");
+const markdownIt = require("markdown-it");
+
+const markdownItVideo = require("markdown-it-video", {
+  youtube: { width: 640, height: 390 },
+  vimeo: { width: 500, height: 281 },
+  vine: { width: 600, height: 600, embed: 'simple' },
+  prezi: { width: 550, height: 400 }
+});
+
+
 module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("src/assets");
@@ -7,20 +18,29 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/data/.nojekyll": ".nojekyll" });
 
 
-  const markdownIt = require("markdown-it");
-  const markdownItVideo = require("markdown-it-video", {
-    youtube: { width: 640, height: 390 },
-    vimeo: { width: 500, height: 281 },
-    vine: { width: 600, height: 600, embed: 'simple' },
-    prezi: { width: 550, height: 400 }
+
+  // Configure markdown
+  const markdownLib = markdownIt({
+    html: true
+  }).use(markdownItVideo);
+  eleventyConfig.setLibrary("md", markdownLib);
+
+
+  // HTML minifier
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+    if (outputPath && outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+
+    return content;
   });
 
-  const options = {
-    html: true
-  };
-
-  const markdownLib = markdownIt(options).use(markdownItVideo);
-  eleventyConfig.setLibrary("md", markdownLib);
 
   return {
     dir: {
